fae83d389ae79df30e34ba2708f95f9b9f9663d2
evajk <c.evakrivec@gmail.com>
2022-09-17 10:22:59 -0700

    class name change

--- src/main/java/mooks.java
+++ src/main/java/mooks.java
@@ -1,4 +1,4 @@
-public class HelloWorldClass {
+public class mooks {
     public static void main(String[] args) {
         System.out.println("Hello world! :)");
         System.out.println("Female Coders Hackathon Linz 2022");
