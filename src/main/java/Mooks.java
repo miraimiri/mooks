6a41a2674a022a102f8bd7afea9f12e4b300840e
miriaimiri <miriam.derenbach@outlook.com>
2022-09-17 13:03:04 +0200

    update main function

---
+++ src/main/java/Mooks.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Mooks {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Mooks.class, args);
+    }
+}
--- src/main/java/mooks.java
+++
@@ -1,6 +0,0 @@
-public class mooks {
-    public static void main(String[] args) {
-        System.out.println("Hello world! :)");
-        System.out.println("Female Coders Hackathon Linz 2022");
-    }
-}
