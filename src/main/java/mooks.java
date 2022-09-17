ef220c853d4318ce06048a26c558aac0de85a62b
Miriam Derenbach <68542210+miraimiri@users.noreply.github.com>
2022-09-17 10:16:28 +0200

    backend structure and main file

---
+++ src/main/java/mooks.java
@@ -0,0 +1,6 @@
+public class HelloWorldClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world! :)");
+        System.out.println("Female Coders Hackathon Linz 2022");
+    }
+}
