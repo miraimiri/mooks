fae3228115d1e088b02948900d41bebf6c999a91
miriaimiri <miriam.derenbach@outlook.com>
2022-09-17 14:28:24 +0200

    adding title search to controller

--- src/main/java/mooksController.java
+++ src/main/java/mooksController.java
@@ -2,6 +2,8 @@ package main.java;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,8 +15,16 @@ public class mooksController {
     }
 
     @GetMapping("/movies/popular")
-    public String getMovieByTitle(){
+    public String getPopularMovies(){
         String uri = "https://imdb-api.com/en/API/MostPopularMovies/k_0w8gobka";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/movie/{title}")
+    public String getMovieByTitle(@PathVariable String title) {
+        String uri = String.format("https://imdb-api.com/en/API/SearchTitle/k_0w8gobka/%s", title);
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         return result;
